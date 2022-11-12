a936022aa4b45d6c1ba455bf7b1470443f389e8e
Cleyson Lima <cleysonph@gmail.com>
2022-11-12 07:18:26 -0300

    feat: implement the get books controller

---
+++ internal/infra/controller/controller.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cleysonph/bookshelf-go/internal/application"
+	"github.com/cleysonph/bookshelf-go/internal/infra/dto"
+	"github.com/cleysonph/bookshelf-go/internal/infra/web"
+)
+
+type WebController interface {
+	Execute(request *web.HttpRequest) *web.HttpResponse
+}
+
+var defaultHeaders = web.Headers{
+	ContentType: "application/json",
+}
+
+func newJsonResponse(statusCode int, body interface{}) *web.HttpResponse {
+	json, err := json.Marshal(body)
+	if err != nil {
+		return handleErrorResponse(err)
+	}
+	return web.NewHttpResponse(statusCode, json, defaultHeaders)
+}
+
+func handleErrorResponse(err error) *web.HttpResponse {
+	switch t := err.(type) {
+	case *application.ApplicationError:
+		e := dto.NewErrorResponse(t, http.StatusInternalServerError)
+		return newJsonResponse(e.Status, e)
+	default:
+		e := dto.NewErrorResponse(err, http.StatusInternalServerError)
+		return newJsonResponse(e.Status, e)
+	}
+}
