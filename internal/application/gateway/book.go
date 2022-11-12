1524398d31ec31df606ea4779aacb44f6dcfdd7a
Cleyson Lima <cleysonph@gmail.com>
2022-11-12 20:06:34 -0300

    feat: add function definition for find book by id in gateway

--- internal/application/gateway/book.go
+++ internal/application/gateway/book.go
@@ -4,5 +4,5 @@ import "github.com/cleysonph/bookshelf-go/internal/domain"
 
 type BookGateway interface {
 	FindAllByTitle(title string) ([]*domain.Book, error)
-	FindById(id int64) (*domain.Book, error)
+	FindById(id string) (*domain.Book, error)
 }
