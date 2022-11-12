68ff2bee55232901a6501d5445dd992401857bb6
Cleyson Lima <cleysonph@gmail.com>
2022-11-12 19:15:48 -0300

    feat: implement usecase find book by id

--- internal/application/usecase/book.go
+++ internal/application/usecase/book.go
@@ -16,3 +16,15 @@ func NewGetBooksUseCase(bookGateway gateway.BookGateway) *GetBooksUseCase {
 func (u *GetBooksUseCase) Execute(title string) ([]*domain.Book, error) {
 	return u.bookGateway.FindAllByTitle(title)
 }
+
+type GetBookUseCase struct {
+	bookGateway gateway.BookGateway
+}
+
+func NewGetBookUseCase(bookGateway gateway.BookGateway) *GetBookUseCase {
+	return &GetBookUseCase{bookGateway}
+}
+
+func (u *GetBookUseCase) Execute(id int64) (*domain.Book, error) {
+	return u.bookGateway.FindById(id)
+}
