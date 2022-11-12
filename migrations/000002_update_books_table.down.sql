dd5e6276826df4c3c4fe7048122b23423d398b86
Cleyson Lima <cleysonph@gmail.com>
2022-11-12 19:09:08 -0300

    chore: create migrations to update books table

---
+++ migrations/000002_update_books_table.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `books` DROP COLUMN `description`;
+ALTER TABLE `books` DROP COLUMN `published_at`;
+ALTER TABLE `books` DROP COLUMN `publisher`;
+ALTER TABLE `books` DROP COLUMN `pages`;
+ALTER TABLE `books` DROP COLUMN `edition`;
